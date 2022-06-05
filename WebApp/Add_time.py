d3ae726e42b877893b1a8b2de080ff774b5a7021
Igor <igor.gaz2004@gmail.com>
2022-06-05 19:33:22 +0300

    some tests

--- WebApp/Add_time.py
+++ WebApp/Add_time.py
@@ -1,6 +1,5 @@
 def Add_time(f1, f2, f3):
-    with open(f1, 'r') as r, open(f2, 'r') as t, open(f3,
-                                                                                                     'w') as nr:
+    with open(f1, 'r') as r, open(f2, 'r') as t, open(f3, 'w') as nr:
         rlines = r.readlines()
         tlines = t.readlines()
         for i in range(len(rlines)):
